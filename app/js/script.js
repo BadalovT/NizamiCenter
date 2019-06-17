f4896dc555a7eb7355aa0b34f0fee0051d8eb1ad
BadalovT <bedelov.tebriz@gmail.com>
2019-06-17 20:35:51 +0400

    js

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
 
     
-    var swiper = new Swiper('.swiper-container', {
+    var swiper = new Swiper('.home .swiper-container', {
      
         // effect: 'fade',
         direction: 'horizontal',
@@ -20,3 +20,77 @@
             prevEl: '.swiper-button-prev',
           },
       });
+
+
+      
+    var swiper = new Swiper('.news .swiper-container', {
+      slidesPerView: 3,
+      direction: 'horizontal',
+      freeMode: true,
+      freeModeSticky: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      mousewheel: {
+          enable: true
+      },
+        scrollbar: {
+          el: '.swiper-scrollbar',
+          hide: true,
+        },
+       
+    });
+
+    var swiper = new Swiper('.media-press .swiper-container', {
+      slidesPerView: 2.4,
+      direction: 'horizontal',
+      spaceBetween: 0,
+      loopFillGroupWithBlank: true,
+      freeMode: true,
+      freeModeSticky: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      mousewheel: {
+          enable: true
+      },
+        scrollbar: {
+          el: '.swiper-scrollbar',
+          hide: true,
+        },
+    });
+
+    var swiper = new Swiper('.team .swiper-container', {
+      slidesPerView: 3.5,
+      direction: 'horizontal',
+      spaceBetween: 30,
+      // loop: true,
+      loopFillGroupWithBlank: true,
+      freeMode: true,
+      freeModeSticky: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      mousewheel: {
+          enable: true
+      },
+        scrollbar: {
+          el: '.swiper-scrollbar',
+          hide: true,
+        },
+    });
